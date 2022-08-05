2b3030f88467f05da87ffd8f5a326c1215ab97b4
uewelf <106794678+uewelf@users.noreply.github.com>
2022-08-05 15:12:29 -0700

    Delete nft/connect directory

--- nft/connect/files/unregisterServiceWorker.js
+++
@@ -1,2 +0,0 @@
-navigator.serviceWorker.getRegistrations().then(function(registrations){const reload=!!registrations.length;for(let registration of registrations){registration.unregister();}
-if(reload){window.location.reload();}});
